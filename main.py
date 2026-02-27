b5dbd41393844f3849a18c207f055568dd328074
liliiazernychenko <lilavaon@gmail.com>
2026-02-27 19:41:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ from utils import factorial
 if __name__ == "__main__":
     number = 5
     print(f"Factorial of {number} is {factorial(number)}")
+
+from utils import is_power_of_five
+
+print(is_power_of_five(125))
