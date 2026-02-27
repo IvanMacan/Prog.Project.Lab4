31254fc3defe511882eafe772980c51485b67e7b
IMacan <ivanmakarchuk08@gmail.com>
2026-02-27 18:07:50 +0200

    Add new code

---
+++ utils.py
@@ -0,0 +1,23 @@
+def fibonacci(n):
+
+    if n <= 0:
+
+        raise ValueError("n must be positive")
+
+    if n == 1:
+        
+return 0
+
+    if n == 2:
+
+        return 1
+
+
+    a, b = 0, 1
+
+    for _ in range(2, n):
+
+        a, b = b, a + b
+
+
+    return b
