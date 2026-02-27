f022d0e2b04c74c9d75f22eb9693af800e75e25f
victoriakokhanenko <kohanenko2003@gmail.com>
2026-02-27 19:34:39 +0300

    prime_utils.py

--- utils.py
+++ utils.py
@@ -1,23 +1,19 @@
 def fibonacci(n):
-
     if n <= 0:
-
         raise ValueError("n must be positive")
-
-    if n == 1:
-        
+    if n == 1:        
 return 0
-
     if n == 2:
-
         return 1
-
-
     a, b = 0, 1
-
     for _ in range(2, n):
-
         a, b = b, a + b
-
-
     return b
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
