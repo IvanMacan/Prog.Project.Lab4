5d259744ca2b70667ff13d2ecca6e15e896559a0
victoriakokhanenko <kohanenko2003@gmail.com>
2026-02-27 20:25:31 +0300

    update_main.py

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ from utils import factorial
 if __name__ == "__main__":
     number = 5
     print(f"Factorial of {number} is {factorial(number)}")
+
+
+from utils import is_prime
+print(is_prime(7))
